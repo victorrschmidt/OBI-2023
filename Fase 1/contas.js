f4f5291167571db3beaa380a758fdad7ff246f31
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2023-06-04 22:49:12 -0300

    Add files via upload

---
+++ Fase 1/contas.js
@@ -0,0 +1,19 @@
+var v, c, min, i, con;
+c = [];
+scanf("%d%d%d%d","v","c[0]","c[1]","c[2]");
+
+con = 0;
+
+ for(i = 0; i < 3; i++){
+    min = Math.min.apply(null, c);
+    var b = c.indexOf(min);
+    if(min > v){
+      break;
+    }
+    else{
+      v -= min;
+      c.splice(b,1);
+      con++;   
+    }
+  }
+printf("%d\n",con);
